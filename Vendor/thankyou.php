9d67a9ec65f4b1571f5d0c0c9f2efea91530191a
Jack Kelleher <117443752@umail.ucc.ie>
2018-03-06 11:30:39 +0000

    added contact form , fixed errors

---
+++ Vendor/thankyou.php
@@ -0,0 +1,59 @@
+<!Doctype HTML>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+              
+                <!--Refrencing stylesheet-->
+              <link rel="stylesheet" href="vendor.css" type="text/css" />
+              
+              <!--title-->
+        <title>Thank You </title>
+        
+        <!-- Navbar code aqquired from https://www.w3schools.com/css/css_navbar.asp-->   
+<style>
+
+
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+}
+
+li {
+    float: left;
+}
+
+li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+li a:hover:not(.active) {
+    background-color: #111;
+}
+
+.active {
+    background-color: #4CAF50;
+}
+</style>
+         
+    </head>
+    
+    <body>
+        
+        <!---Adding links to the nav bar-->
+            <ul>
+  <li><a href="Cloudservices.html">Mainpage</a></li>
+  <li><a href="aboutus.html">About us</a></li>
+  <li><a href="aboutcloud.html">About cloud</a></li>
+  <li><a href="products.html">Products</a></li>
+  <li style="float:right"><a class="active" href="../Homepage.html">Homepage</a></li>
+</ul>
+        
+      <h1> Thank you , your query has been recieved</h1>
+    </body>
